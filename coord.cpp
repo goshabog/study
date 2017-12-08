2b0c1135f85c1095fbffadfabad974cc654e10c9
goshabog <goshabog@yahoo.com>
2017-12-08 16:29:04 +0300

    add more ooerations

--- coord.cpp
+++ coord.cpp
@@ -10,6 +10,27 @@ class coord
     void get_xy(int &a, int &b) { a = x; b = y; }
     void print() { cout << "x = " << x << " y = " << y << endl; }
     coord operator+(coord ob2);
+    coord operator+(int i);
+    coord operator+();
+
+    coord operator-(coord ob2);
+    coord operator-();
+
+    coord operator=(coord ob2);
+
+    coord operator*(coord ob2);
+    coord operator*(int i);
+
+    int operator==(coord ob2);
+    int operator&&(coord ob2);
+    int operator<(coord ob2);
+    int operator>(coord ob2);
+
+    coord operator++();
+    coord operator++(int notused);
+
+    coord operator--();
+    coord operator--(int notused);
 };
 
 coord coord:: operator+(coord ob2)
@@ -20,9 +41,111 @@ coord coord:: operator+(coord ob2)
     return temp;
 }
 
+coord coord:: operator+()
+{
+    if (x <0) x = -x;
+    if (y <0) y = -y;
+    return *this;
+}
+
+coord coord:: operator-(coord ob2)
+{
+    coord temp;
+    temp.x = x - ob2.x;
+    temp.y = y - ob2.y;
+    return temp;
+}
+
+coord coord:: operator-()
+{
+    x = -x;
+    y = -y;
+    return *this;
+}
+
+coord coord:: operator=(coord ob2)
+{
+    x = ob2.x;
+    y = ob2.y;
+    return *this;
+}
+
+coord coord:: operator+(int i)
+{
+    coord temp;
+    temp.x = x + i;
+    temp.y = y + i;
+    return temp;
+}
+
+coord coord:: operator*(coord ob2)
+{
+    coord temp;
+    temp.x = x * ob2.x;
+    temp.y = y * ob2.y;
+    return temp;
+}
+
+coord coord:: operator*(int i)
+{
+    coord temp;
+    temp.x = x * i;
+    temp.y = y * i;
+    return temp;
+}
+
+int coord::operator==(coord ob2)
+{
+    return (x == ob2.x) && (y == ob2.y);
+}
+
+int coord::operator&&(coord ob2)
+{
+    return (x && ob2.x) && (y && ob2.y);
+}
+
+int coord::operator<(coord ob2)
+{
+    return (x < ob2.x) && (y < ob2.y);
+}
+
+int coord::operator>(coord ob2)
+{
+    return (x > ob2.x) && (y > ob2.y);
+}
+
+coord coord::operator++()
+{
+    x++;
+    y++;
+    return *this;
+}
+
+coord coord::operator++(int notused)
+{
+    x++;
+    y++;
+    return *this;
+}
+
+coord coord::operator--()
+{
+    x--;
+    y--;
+    return *this;
+}
+
+coord coord::operator--(int notused)
+{
+    x--;
+    y--;
+    return *this;
+}
+
 int main()
 {
     coord o1(10,10), o2(5,3), o3;
+    coord o4(10,10), o5(0,0);
     cout << "o1: ";
     o1.print();
 
@@ -33,5 +156,63 @@ int main()
     cout << "o3 = o1+o2: ";
     o3.print();
 
+    o3 = o1 - o2;
+    cout << "o3 = o1-o2: ";
+    o3.print();
+
+    o3 = o1;
+    cout << "o3 = o1: ";
+    o3.print();
+
+    o3 = o1 + 100;
+    cout << "o3 = o1+100: ";
+    o3.print();
+
+    o3 = o1 * o2;
+    cout << "o3 = o1*o2: ";
+    o3.print();
+
+    o3 = o1 * 100;
+    cout << "o3 = o1*100: ";
+    o3.print();
+
+    if(o1 == o2)
+       cout << "o1 == o2\n";
+    else
+       cout << "o1 != o2\n";
+
+    if(o1 == o4)
+       cout << "o1 == o4\n";
+    else
+       cout << "o4 != o4\n";
+
+    if(o1 && o2)
+       cout << "o1 && o2 is true\n";
+    else
+       cout << "o1 && o2 is false\n";
+
+    if(o1 && o5)
+       cout << "o1 && o5 is true\n";
+    else
+       cout << "o1 && o5 is false\n";
+
+    if(o1 < o2)
+       cout << "o1 < o2 is true\n";
+    else
+       cout << "o1 < o2 is false\n";
+
+    if(o1 > o5)
+       cout << "o1 > o5 is true\n";
+    else
+       cout << "o1 > o5 is false\n";
+
+    (++o1).print();
+    (o1++).print();
+
+    (--o2).print();
+    (o2--).print();
+
+    (-o1).print();
+    (+o1).print();
     return 0;
 }   
